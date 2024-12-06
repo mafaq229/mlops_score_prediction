36d9849098521a0feb02f6323b2591008488682e
Muhammad Afaq <muhammadafaq1999@gmail.com>
2024-12-06 22:08:07 +0400

    Exploratory Data Analysis + sample model training on notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -21,4 +21,10 @@ class CustomException(Exception):
         return self.error_message
 
 
+if __name__ == "__main__": # test
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)
     
